6e3d11552d773c77bf3a7869b391948bc1704d7a
Sergeiproger <czwetkow.serega@yandex.ru>
2020-04-18 21:34:32 +0300

    Version 'Moving ball'

--- balls.py
+++ balls.py
@@ -6,17 +6,15 @@ HEIGHT = 200
 TICK_RATE = 50 
 
 class Ball:
-    def __init__(self, x, y, R):
+    def __init__(self, x, y, R, color):
+        global ball_id
         self.x = x
         self.y = y
         self.R = R
-        self.color = '#990f99'
-        canvas.create_oval(x - R, y - R, x + R, y + R, fill = self.color)
+        self.color = color
+        ball_id = canvas.create_oval(x - R, y - R, x + R, y + R, fill = self.color)
     def move(self, dx, dy):
-        global x, y, R
-        canvas.move(canvas.create_oval(x - R, y - R, x + R, y + R, fill = self.color), dx, dy) # переработать
-        x += dx
-        y += dy
+        canvas.move(ball_id, dx, dy)
 
 def canvas_click_handler(event):
     print('x =', event.x, 'y =', event.y)
@@ -25,7 +23,7 @@ def tick():
     global x, y
     """ Обработчик времени
     """
-    ball_1.move(1, 1)
+    ball_1.move(5, 0)
     root.after(TICK_RATE, tick)
 
 def main():
@@ -40,7 +38,7 @@ def main():
     R = randint(20, 50)
     x = randint(R, WIDTH - R)
     y = randint(R, HEIGHT - R)
-    ball_1 = Ball(x, y, R)
+    ball_1 = Ball(x, y, R, '#ff3333')
 
     tick()
     root.mainloop()
