289901bf438103a950688bc56ab9c7c69928cedc
Sergeiproger <czwetkow.serega@yandex.ru>
2020-04-18 23:44:05 +0300

    Ball with collision. TODO: Refactoring

--- balls.py
+++ balls.py
@@ -3,42 +3,54 @@ from random import randint
 
 WIDTH = 300
 HEIGHT = 200
-TICK_RATE = 50 
+TICK_RATE = 50
 
+dx, dy = 1, 1
 class Ball:
-    def __init__(self, x, y, R, color):
-        global ball_id
+    def __init__(self, x, y, R, color, speeed):
+        global ball_id, dx,dy
         self.x = x
         self.y = y
         self.R = R
         self.color = color
-        ball_id = canvas.create_oval(x - R, y - R, x + R, y + R, fill = self.color)
+        self.speed = speeed
+        ball_id =\
+            canvas.create_oval(x - R, y - R, x + R, y + R, fill=self.color)
+        dx, dy = speeed, speeed
+
     def move(self, dx, dy):
         canvas.move(ball_id, dx, dy)
 
-def canvas_click_handler(event):
-    print('x =', event.x, 'y =', event.y)
+    def collision_handler(self):
+        global x, y, R, dx, dy
+        x += dx
+        y += dy
+        if x + R > WIDTH or x - R < 0:
+            dx = -dx
+        if y + R > HEIGHT or y - R < 0:
+            dy = -dy
 
 def tick():
-    global x, y
+    global dx, dy
     """ Обработчик времени
     """
-    ball_1.move(5, 0)
+    ball_1.collision_handler()
+    ball_1.move(dx, dy)
     root.after(TICK_RATE, tick)
+
 
 def main():
     global root, canvas
-    global x, y, z, ball_1, R
+    global ball_1, x, y, R
     root = tk.Tk()
     root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
     canvas = tk.Canvas(root)
     canvas.pack()
-    canvas.bind('<Button-1>', canvas_click_handler)
 
     R = randint(20, 50)
     x = randint(R, WIDTH - R)
     y = randint(R, HEIGHT - R)
-    ball_1 = Ball(x, y, R, '#ff3333')
+    ball_1 = Ball(x, y, R, '#ff3333', 5)
 
     tick()
     root.mainloop()
