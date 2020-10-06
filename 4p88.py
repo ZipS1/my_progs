8d7ea2bd05ba008124c1979ee4b3fd203000feb7
ZipS <czwetkow.serega@yandex.ru>
2020-10-06 09:18:53 +0300

    dz

---
+++ 4p88.py
@@ -0,0 +1,22 @@
+def sort(i):
+	return i[1]
+
+with open('4p88_in.txt', 'r', encoding='utf-8') as f:
+	words = f.readlines()
+
+each = []
+counting = []
+
+for i in words:
+	if i not in each:
+		each.append(i)
+		arr = [i, words.count(i)]
+		counting.append(arr)
+
+counting.sort(key=sort, reverse = True)
+
+with open('4p88_out.txt', 'w', encoding='utf-8') as f:
+	for el in counting:
+		for i in range(el[1] - 1):
+			f.write(el[0])  
+
