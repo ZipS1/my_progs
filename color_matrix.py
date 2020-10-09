89b6f16d7889bfbc96507738f72d7dbd477f51c8
ZipS1 <czwetkow.serega@ayandex.ru>
2020-10-09 20:40:36 +0300

    TODO: connect it with matrix_generate

--- color_matrix.py
+++ color_matrix.py
@@ -1,18 +1,18 @@
-def matrix_output():
-	for i in matrix:
-		print(i)
-	quit()
+def print_matrix():
+    for i in matrix:
+        print(i)
 
 import random
 
 matrix = [[0,1,0,1,1],
-		  [1,1,1,2,2],
-		  [0,1,0,2,2],
-		  [3,3,1,2,2],
-		  [0,1,1,0,0]]
+          [1,1,1,2,2],
+          [0,1,0,2,2],
+          [3,3,1,2,2],
+          [0,1,1,0,0]]
 
 # matrix[y][x]
 
+print_matrix()
 start_point = input("Enter starting point (x, y): ").split()
 start_point = [int(i) - 1 for i in start_point]
 start_x, start_y = start_point
@@ -21,7 +21,7 @@ start_x, start_y = start_point
 color = matrix[start_y][start_x]
 new_color = int(input("Enter your color (0-3): "))
 if color == new_color:
-	matrix_output()
+    matrix_output()
 
 c_point = (start_y, start_x)
 
@@ -30,28 +30,29 @@ c_point = (start_y, start_x)
 area = [c_point]
 
 while True:
-	y = c_point[0]
-	x = c_point[1]
-	matrix[y][x] = new_color
+    y = c_point[0]
+    x = c_point[1]
+    matrix[y][x] = new_color
 
-	if x - 1 >= 0:
-		if matrix[y][x - 1] == color:
-			area.append((y, x - 1))
-	if x + 1 <= 4:
-		if matrix[y][x + 1] == color:
-			area.append((y, x + 1))
-	if y - 1 >= 0:
-		if matrix[y - 1][x] == color:
-			area.append((y - 1, x))
-	if y + 1 <= 4:
-		if matrix[y + 1][x] == color:
-			area.append((y + 1, x))
-	area.remove(c_point)
+    if x - 1 >= 0:
+        if matrix[y][x - 1] == color:
+            area.append((y, x - 1))
+    if x + 1 <= 4:
+        if matrix[y][x + 1] == color:
+            area.append((y, x + 1))
+    if y - 1 >= 0:
+        if matrix[y - 1][x] == color:
+            area.append((y - 1, x))
+    if y + 1 <= 4:
+        if matrix[y + 1][x] == color:
+            area.append((y + 1, x))
+    area.remove(c_point)
 
-	if area == []:
-		matrix_output()
-	else:
-		c_point = area[0]
+    if area == []:
+        print_matrix()
+        quit()
+    else:
+        c_point = area[0]
 
 
 
